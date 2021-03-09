16d4f7e1a95aaa6b8c9e111d8e5fdf3ee3c86554
Ee-Hsin <eehsinkok777@gmail.com>
2021-03-09 20:53:45 +0800

    Added controllers, refactored Portfolio routes

---
+++ controllers/portfolio.js
@@ -0,0 +1,94 @@
+const Stock = require('../models/stock');
+const compare = require('../utils/compareFunction')
+
+module.exports.index = async (req, res) => {
+    const stocks = await Stock.find({});
+    //Add a sorting method here.
+    stocks.sort(compare);
+    res.render('portfolio/index', {stocks});
+};
+
+module.exports.renderNewStockForm = async (req, res) => {
+    res.render('portfolio/new');
+};
+
+module.exports.createNewStock = async (req, res) => {
+    const stock = new Stock(req.body.stock);
+    stock.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    await stock.save();
+    req.flash('success', 'Successfully added Stock')
+    res.redirect(`/portfolio/${stock._id}`)
+};
+
+module.exports.showStock = async (req, res) => {
+    const stock = await Stock.findById(req.params.id);
+    if (!stock){
+        req.flash('error', 'Cannot find that Stock!');
+        return res.redirect('/portfolio')
+    }
+    //Adding financials to stock
+    stock.financials = await stock.financialInfo;
+
+    res.render('portfolio/show', {stock});
+};
+
+module.exports.sendChartData= async (req,res) => {
+    const stock = await Stock.findById(req.params.id);
+    const candleData = await stock.oneYearCandleData;
+    res.send(candleData);
+};
+
+module.exports.sendReturns = async (req,res) => {
+    const stock = await Stock.findById(req.params.id);
+    const currentPriceAndReturns = await stock.currentPriceAndReturns;
+    res.send(currentPriceAndReturns);
+};
+
+module.exports.sendReturnsYTD = async (req,res) => {
+    const stock = await Stock.findById(req.params.id);
+    const returnsYTD = await stock.returnsYTD;
+    res.send(returnsYTD);
+};
+
+module.exports.sendIV = async (req,res) => {
+    const stock = await Stock.findById(req.params.id);
+    const IV = await stock.IV;
+    res.send(IV);
+};
+
+module.exports.renderEditForm = async (req, res,) => {
+    const stock = await Stock.findById(req.params.id);
+    if (!stock){
+        req.flash('error', 'Cannot find that Stock!');
+        return res.redirect('/portfolio')
+    }
+    res.render('portfolio/edit', { stock });
+};
+
+module.exports.updateStock = async (req, res) => {
+    const { id } = req.params;
+    const stock = await Stock.findByIdAndUpdate(id, { ...req.body.stock });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    stock.images.push(...imgs);
+    await stock.save();
+
+    //For deleting images
+    if (req.body.deleteImages) { 
+        for (let filename of req.body.deleteImages) { //loop through images to remove them from cloudinary
+            await cloudinary.uploader.destroy(filename);
+        }
+        //no need for loop to iterate through for this one, it will pull all images where filename is in the req.body.deleteImages array.
+        await stock.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+
+    req.flash('success', 'Successfully edited Stock')
+    res.redirect(`/portfolio/${stock._id}`)
+};
+
+module.exports.deleteStock = async (req, res) => {
+    const { id } = req.params;
+    //I can add functionality here to findId of stock, and delete all images associated with the stock.
+    await Stock.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted Stock')
+    res.redirect('/portfolio');
+};
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,114 +1,40 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const portfolio = require('../controllers/portfolio');
 const catchAsync = require('../utils/catchAsync');
-
-const compare = require('../utils/compareFunction')
-
+const { validateStock, isLoggedIn, isAdmin } = require('../middleware.js');
 const multer  = require('multer');
 const { storage, cloudinary } = require('../cloudinary');
 const upload = multer({ storage });
 
-const { validateStock, isLoggedIn, isAdmin } = require('../middleware.js');
-
-const ExpressError = require('../utils/ExpressError');
+//No need these two after refactoring:
 const Stock = require('../models/stock');//can ship this off to individual route files.
-const { response } = require('express');
+const compare = require('../utils/compareFunction')
 
-router.get('/', catchAsync(async (req, res) => {
-    const stocks = await Stock.find({});
-    //Add a sorting method here.
-    stocks.sort(compare);
+router.route('/')
+    .get(catchAsync(portfolio.index))
+    .post(isLoggedIn, isAdmin, upload.array('image'), validateStock, catchAsync(portfolio.createNewStock))
 
-    res.render('portfolio/index', {stocks})
-}));
+router.get('/new', isLoggedIn, isAdmin, portfolio.renderNewStockForm);
 
-router.get('/new', isLoggedIn, isAdmin, (req, res,) => {
-    res.render('portfolio/new');
-});
+router.route('/:id')
+    .get(catchAsync(portfolio.showStock))
+    .put(isLoggedIn, isAdmin, upload.array('image'), validateStock, catchAsync(portfolio.updateStock))
+    .delete(isLoggedIn, isAdmin, catchAsync(portfolio.deleteStock));
 
-router.post('/', isLoggedIn, isAdmin, upload.array('image'), validateStock, catchAsync(async (req, res) => {
-    const stock = new Stock(req.body.stock);
-    stock.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    await stock.save();
-    req.flash('success', 'Successfully added Stock')
-    res.redirect(`/portfolio/${stock._id}`)
-}));
+//TO render edit form:
+router.get('/:id/edit', isLoggedIn, isAdmin, catchAsync(portfolio.renderEditForm));
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const stock = await Stock.findById(req.params.id);
-    if (!stock){
-        req.flash('error', 'Cannot find that Stock!');
-        return res.redirect('/portfolio')
-    }
-    //Adding financials to stock
-    stock.financials = await stock.financialInfo;
-
-    res.render('portfolio/show', {stock});
-}));
-
+//SENDING BACK DATA STUFF:
 //My API Response to client side to send Candlestick data
-router.get('/:id/getChartData', catchAsync(async (req,res) => {
-    const stock = await Stock.findById(req.params.id);
-    const candleData = await stock.oneYearCandleData;
-    res.send(candleData);
-}))
-
+router.get('/:id/getChartData', catchAsync(portfolio.sendChartData));
 //My API Response to client side to send Returns data
-router.get('/:id/getCurrentPriceAndReturns', catchAsync(async (req,res) => {
-    const stock = await Stock.findById(req.params.id);
-    const currentPriceAndReturns = await stock.currentPriceAndReturns;
-    res.send(currentPriceAndReturns);
-}))
+router.get('/:id/getCurrentPriceAndReturns', catchAsync(portfolio.sendReturns));
 //My API Response to client side to send ReturnsYTD data
-router.get('/:id/getReturnsYTD', catchAsync(async (req,res) => {
-    const stock = await Stock.findById(req.params.id);
-    const returnsYTD = await stock.returnsYTD;
-    res.send(returnsYTD);
-}))
-
+router.get('/:id/getReturnsYTD', catchAsync(portfolio.sendReturnsYTD));
 //My API Response to client side to send IV data
-router.get('/:id/getIV', catchAsync(async (req,res) => {
-    const stock = await Stock.findById(req.params.id);
-    const IV = await stock.IV;
-    res.send(IV);
-}))
+router.get('/:id/getIV', catchAsync(portfolio.sendIV));
 
-router.get('/:id/edit', isLoggedIn, isAdmin, catchAsync(async (req, res,) => {
-    const stock = await Stock.findById(req.params.id);
-    if (!stock){
-        req.flash('error', 'Cannot find that Stock!');
-        return res.redirect('/portfolio')
-    }
-    res.render('portfolio/edit', { stock });
-}));
-
-router.put('/:id', isLoggedIn, isAdmin, upload.array('image'), validateStock, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const stock = await Stock.findByIdAndUpdate(id, { ...req.body.stock });
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    stock.images.push(...imgs);
-    await stock.save();
-
-    //For deleting images
-    if (req.body.deleteImages) { 
-        for (let filename of req.body.deleteImages) { //loop through images to remove them from cloudinary
-            await cloudinary.uploader.destroy(filename);
-        }
-        //no need for loop to iterate through for this one, it will pull all images where filename is in the req.body.deleteImages array.
-        await stock.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
-    }
-
-    req.flash('success', 'Successfully edited Stock')
-    res.redirect(`/portfolio/${stock._id}`)
-}));
-
-router.delete('/:id', isLoggedIn, isAdmin, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    //I can add functionality here to findId of stock, and delete all images associated with the stock.
-    await Stock.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted Stock')
-    res.redirect('/portfolio');
-}));
 
 // //Error Handler (don't need, handled in app.js)
 // router.all('*', (req,res) => {
